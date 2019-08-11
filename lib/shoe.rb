4f023d15d9f308cac44ccb700223ad0d97fee53f
Brian Wong <bwong92@gmail.com>
2019-08-11 20:02:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 require 'pry'
 class Shoe
-  
-  attr_reader :brand
 
-  BRANDS = []
-
+  BRANDS =[]
   def initialize(brand)
-   @brand = brand
-   BRANDS << brand if !BRANDS.include?(brand)
- end
+    @brand = brand
+    BRANDS << brand
+  end
 
 
 
